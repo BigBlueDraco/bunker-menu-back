aadd4da4835dd65e31ab4f66f72165a3f111d77d
BigBlueDraco <Dafarlog@gmail.com>
2023-06-15 14:41:27 +0300

    added sub-category

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { CategoryModule } from './category/category.module';
+import { SubCategoryModule } from './sub-category/sub-category.module';
 
 @Module({
-  imports: [PrismaModule, CategoryModule],
+  imports: [PrismaModule, CategoryModule, SubCategoryModule],
   controllers: [],
   providers: [],
 })
