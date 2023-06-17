b89e9f4ddc6b172db980bf30ef40c0a81945468f
BigBlueDraco <Dafarlog@gmail.com>
2023-06-17 18:27:03 +0300

    added exeptions

--- src/hookah/service/hookah.service.ts
+++ src/hookah/service/hookah.service.ts
@@ -2,30 +2,51 @@ import { Injectable } from '@nestjs/common';
 import { CreateHookahInput } from '../dto/create-hookah.input';
 import { UpdateHookahInput } from '../dto/update-hookah.input';
 import { PrismaService } from 'src/prisma/service/prisma.service';
+import { GraphQLError } from 'graphql';
 
 @Injectable()
 export class HookahService {
   constructor(private readonly prosmaService: PrismaService) {}
-  create(createHookahInput: CreateHookahInput) {
-    return this.prosmaService.hookah.create({ data: { ...createHookahInput } });
+  async create(createHookahInput: CreateHookahInput) {
+    const { name, ...rest } = createHookahInput;
+    const exsitUser = await this.prosmaService.hookah.findUnique({
+      where: { name: createHookahInput.name.toLowerCase() },
+    });
+    if (exsitUser) {
+      throw new GraphQLError(
+        `Hookah with name: "${createHookahInput.name}" alredy exist`,
+      );
+    }
+
+    return await this.prosmaService.hookah.create({
+      data: { name: name.toLowerCase(), ...rest },
+    });
   }
 
-  findAll() {
-    return this.prosmaService.hookah.findMany();
+  async findAll() {
+    return await this.prosmaService.hookah.findMany();
   }
 
-  findOne(id: number) {
-    return this.prosmaService.hookah.findUniqueOrThrow({ where: { id: id } });
+  async findOne(id: number) {
+    const hookah = await this.prosmaService.hookah.findUnique({
+      where: { id: id },
+    });
+    if (!hookah) {
+      throw new GraphQLError(`Hookah with ${id} dosen't exist`);
+    }
+    return hookah;
   }
 
-  update(id: number, updateHookahInput: UpdateHookahInput) {
-    return this.prosmaService.hookah.update({
+  async update(id: number, updateHookahInput: UpdateHookahInput) {
+    await this.findOne(id);
+    return await this.prosmaService.hookah.update({
       where: { id },
       data: { ...updateHookahInput },
     });
   }
 
-  remove(id: number) {
-    return this.prosmaService.hookah.delete({ where: { id: id } });
+  async remove(id: number) {
+    await this.findOne(id);
+    return await this.prosmaService.hookah.delete({ where: { id: id } });
   }
 }
