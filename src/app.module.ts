a6ec903f1c1ff8243dbedb69df86f8b98f875612
BigBlueDraco <Dafarlog@gmail.com>
2023-06-17 12:21:26 +0300

    Init graphQl playgrond

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { CategoryModule } from './category/category.module';
 import { SubCategoryModule } from './sub-category/sub-category.module';
 import { AssortmentModule } from './assortment/assortment.module';
 import { HookahModule } from './hookah/hookah.module';
+import { ConfigModule } from '@nestjs/config/dist';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [PrismaModule, CategoryModule, SubCategoryModule, AssortmentModule, HookahModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.ggl',
+      sortSchema: true,
+      playground: true,
+    }),
+    PrismaModule,
+    CategoryModule,
+    SubCategoryModule,
+    AssortmentModule,
+    HookahModule,
+  ],
   controllers: [],
   providers: [],
 })
