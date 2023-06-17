121539fdb7f8d64685516e4d2881d526de4be5f2
Gavazadd <gavazadd@gmail.com>
2023-06-17 13:37:06 +0300

    Update category dto

--- src/category/dto/create-category.input.ts
+++ src/category/dto/create-category.input.ts
@@ -3,5 +3,5 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 @InputType()
 export class CreateCategoryInput {
   @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  name: string;
 }
