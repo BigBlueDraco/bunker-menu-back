b2d93c51767aa0ccf20fa307337c8e9a168afcc5
Gavazadd <gavazadd@gmail.com>
2023-06-18 15:05:47 +0300

    Rewrite tests

--- src/category/resolver/category.resolver.spec.ts
+++ src/category/resolver/category.resolver.spec.ts
@@ -3,15 +3,16 @@ import { CategoryResolver } from './category.resolver';
 import { CategoryService } from '../service/category.service';
 import { Category } from '../entities/category.entity';
 import {PrismaService} from "../../prisma/service/prisma.service";
+import {PrismaModule} from "../../prisma/prisma.module";
 
 const categoriesMock = [
   {
-    id: 26,
-    name: 'Test3',
-  },
-  {
     id: 1,
     name: 'Test',
+  },
+  {
+    id: 39,
+    name: 'Test3',
   },
   {
     id: 2,
@@ -23,20 +24,23 @@ const categoriesMock = [
 describe('CategoryResolver', () => {
   let categoryResolver: CategoryResolver;
   let categoryService: CategoryService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CategoryResolver, CategoryService, PrismaService],
+      imports:[PrismaModule]
     }).compile();
 
     categoryResolver = module.get<CategoryResolver>(CategoryResolver);
     categoryService = module.get<CategoryService>(CategoryService);
+    prismaService = module.get<PrismaService>(PrismaService)
   });
 
   describe('createCategory', () => {
     it('should create a new category', async () => {
-      const expectedResult = categoriesMock[0]
-      const result = await categoryResolver.createCategory(categoriesMock[0]);
+      const expectedResult = categoriesMock[1]
+      const result = await categoryResolver.createCategory(categoriesMock[1]);
       expect(result).toEqual(expectedResult);
     });
   });
@@ -61,10 +65,10 @@ describe('CategoryResolver', () => {
     it('should update a category', async () => {
       const newName = 'TestName'
       const expectedResult: Category =   {
-            id: categoriesMock[0].id,
+            id: categoriesMock[1].id,
             name:newName,
           }
-      const result = await categoryResolver.updateCategory({id:categoriesMock[0].id, name: newName});
+      const result = await categoryResolver.updateCategory({id:categoriesMock[1].id, name: newName});
       expect(result).toEqual(expectedResult);
     });
   });
@@ -72,10 +76,10 @@ describe('CategoryResolver', () => {
   describe('removeCategory', () => {
     it('should remove a category by id', async () => {
       const expectedResult: Category =  {
-        id: categoriesMock[0].id,
+        id: categoriesMock[1].id,
         name:'TestName',
       }
-      const result = await categoryResolver.removeCategory(categoriesMock[0].id);
+      const result = await categoryResolver.removeCategory(categoriesMock[1].id);
       expect(result).toEqual(expectedResult);
     });
   });
