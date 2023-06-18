eeb6dcc8d043c26543f68a65ff203e79dd6f1956
Gavazadd <gavazadd@gmail.com>
2023-06-18 19:35:37 +0300

    Change category.service spec file

--- src/category/service/category.service.spec.ts
+++ src/category/service/category.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
+import {PrismaModule} from "../../prisma/prisma.module";
 
 describe('CategoryService', () => {
   let service: CategoryService;
@@ -7,6 +8,7 @@ describe('CategoryService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CategoryService],
+      imports:[PrismaModule]
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
